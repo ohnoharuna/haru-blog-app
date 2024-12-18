43348fab9e8d69ccf7b42cea768c88dd03390b8d
ohnoharuna <>
2024-12-18 11:02:28 +0900

    Relationshipsテーブルを作成

---
+++ test/models/relationship_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id           :integer          not null, primary key
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  follower_id  :integer          not null
+#  following_id :integer          not null
+#
+# Indexes
+#
+#  index_relationships_on_follower_id   (follower_id)
+#  index_relationships_on_following_id  (following_id)
+#
+require 'test_helper'
+
+class RelationshipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
